b8f873a2f2e1a204f847b579af3f1eed5d8b14ce
Mawadda <mawadaamir99@gmail.com>
2024-06-30 19:16:00 +0300

    Web Automation Task

--- src/test/java/CraeteCourseStepdefinition.java
+++ src/test/java/CraeteCourseStepdefinition.java
@@ -11,11 +11,13 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.testng.Assert.assertTrue;
 
 import java.time.Duration;
 
 public class CraeteCourseStepdefinition {
     WebDriver driver;
+    String course;
 
     @Before
     public void setUp() {
@@ -27,13 +29,11 @@ public class CraeteCourseStepdefinition {
     public void goToWebsite(String link) {
         driver.navigate().to(link);
 
-
-
     }
 
     @And("^User enter (.*) and (.*)$")
     public void enterEmailAndPassword(String email, String password) {
-        driver.navigate().to("https://swinji.azurewebsites.net");
+
         driver.findElement(By.id("Email")).sendKeys(email);
         driver.findElement(By.id("inputPassword")).sendKeys(password);
         driver.findElement(By.id("inputPassword")).submit();
@@ -42,22 +42,25 @@ public class CraeteCourseStepdefinition {
 
     @And("Open Courses Page")
     public void OpenCoursesPage() throws InterruptedException {
-        Thread.sleep(5000);
         driver.findElement(By.id("btnMyCoursesList")).click();
 
     }
 
     @When("^User Click on Add Course Button And Enter \"(.*)\" as the name, \"(.*)\" as Year and \"(.*)\" as Teacher of course$")
     public void addCourse(String courseName, String year, String teacherName) throws InterruptedException {
+        course = courseName ;
         Thread.sleep(2000);
-        driver.findElement(By.id("btnMyCoursesList")).click();
-
-        Select select=new Select(driver.findElement(By.id("txtCourseName")));
-
         driver.findElement(By.id("btnListAddCourse")).click();
-        driver.findElement(By.id("txtCourseName")).sendKeys("Maths");
-        select.selectByVisibleText("3");
-        driver.findElement(By.cssSelector("h5[title=\"Test Teacher\"]")).click();
+       WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("courseGrade")));
+
+        Select select=new Select(driver.findElement(By.id("courseGrade")));
+
+
+        driver.findElement(By.id("txtCourseName")).sendKeys(courseName);
+        select.selectByVisibleText(year);
+        driver.findElement(By.id("teacherOnBehalf")).click();
+        driver.findElement(By.cssSelector("h5[title=\"" + teacherName +"\"]")).click();
     }
 
     @And("User click create")
@@ -67,11 +70,16 @@ public class CraeteCourseStepdefinition {
 
     }
    @Then("Course should be appear in courses List")
-   public void AppearCourse(){
+   public void AppearCourse() throws InterruptedException {
+        Thread.sleep(2000);
+       String courseName = driver.findElement(By.id("courseNameView")).getText();
+       assertTrue(courseName.contains(course), "Course Not Added");
+
+
 
    }
     @After
     public void Down() {
-      //  driver.quit();
+      driver.quit();
     }
 }
